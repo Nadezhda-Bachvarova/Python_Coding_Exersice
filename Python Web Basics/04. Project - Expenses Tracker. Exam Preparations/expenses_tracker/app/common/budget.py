916f2e944e6900ecc9ac204c2a43af7c3577565e
Nadezhda-Mihaylova <64796417+Nadezhda-Mihaylova@users.noreply.github.com>
2020-11-03 13:57:24 +0200

    solution

---
+++ Python Web Basics/04. Project - Expenses Tracker. Exam Preparations/expenses_tracker/app/common/budget.py
@@ -0,0 +1,8 @@
+from app.models import Profile, Expense
+
+
+def calculate_budget_left(profile, expenses):
+    expenses_cost = sum(expense.price for expense in expenses)
+    budget_left = profile.budget - expenses_cost
+    return budget_left
+
---
+++ Python Web Basics/04. Project - Expenses Tracker. Exam Preparations/expenses_tracker/app/forms/common.py
@@ -0,0 +1,8 @@
+class DisabledFormMixin():
+    def __init__(self):
+        for (_, field) in self.fields.items():
+            field.widget.attrs['disabled'] = True
+            field.widget.attrs['readonly'] = True
+
+
+
